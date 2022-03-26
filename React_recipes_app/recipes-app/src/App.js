689f0e4ce2f0816d9a71ef2863bc2b14beb921ce
Alex Francisco Tinaya <alexbcn98@gmail.com>
2022-03-26 14:09:22 +0100

    primera request a la API de recetas

--- React_recipes_app/recipes-app/src/App.js
+++ React_recipes_app/recipes-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "daf1ab73";
+  const APP_KEY = "219392c8d71b4c379437525eef7faef3";
+  const req_example = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    
+    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_KEY}&query=pasta&maxFat=25&number=2`);
+    const data = await response.json();
+    console.log(data);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
