e36c7c5ac420b4ee1893aef764efae3eb91461c6
Alex Francisco Tinaya <alexbcn98@gmail.com>
2022-03-31 21:43:55 +0200

    app completada

--- React_recipes_app/recipes-app/src/App.js
+++ React_recipes_app/recipes-app/src/App.js
@@ -4,34 +4,55 @@ import Recipe from "./Recipe";
 
 const App = () => {
   const APP_ID = "daf1ab73";
-  const APP_KEY = "219392c8d71b4c379437525eef7faef3";
+  const APP_KEY = "d6b6e23fe9e54d588ba770494ec6d406";
   const req_example = 
   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState(""); // query to make to the API
 
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     
-    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_KEY}&query=pasta&maxFat=25&number=2`);
+    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${APP_KEY}&query=${query}&maxFat=30&addRecipeInformation=true&number=5`);
     const data = await response.json();
     // Change our array of recipes
     console.log(data.results);
     setRecipes(data.results);
   }
 
+  // Change search value throught the search input
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    // Set the query to the value of input
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      <div className="recipes">
       { recipes.map(recipe => (
-        <Recipe key={recipe.id} title={recipe.title} image={recipe.image} calories={recipe.nutrition.nutrients[0].amount}/>
+        <Recipe 
+          key={recipe.id} 
+          title={recipe.title} 
+          image={recipe.image} 
+          calories={recipe.nutrition.nutrients[0].amount}
+          ingredients={recipe.missedIngredients}/>
       ))}
+      </div>
     </div>
   );
 }
--- React_recipes_app/recipes-app/src/Recipe.js
+++ React_recipes_app/recipes-app/src/Recipe.js
@@ -1,12 +1,13 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 
 const Recipe = ({title, image, calories}) => {
     return (
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <p>Calories: {calories}</p>
-            <img src={image} alt={title}/>
+            <img className={style.image} src={image} alt={title}/>
         </div>
     );
 }
